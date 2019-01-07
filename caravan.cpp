88ee9f3a1d32f6d333247807d77fb0ddbd6f5b5a
Jan Kaufmann <jan.kaufmann@liwest.at>
2019-01-07 12:24:21 +0100

    no linker errors

--- caravan.cpp
+++ caravan.cpp
@@ -13,38 +13,79 @@
 
 #include "caravan.h"
 
+/**
+* Initialisiert eine Karawane. D. h., dass es alle in der Karawane befindlichen Tiere aus der
+* Karawane entfernt und die Karawane als leer kennzeichnet.
+* @param caravan Die Karawane, welche initialisiert werden soll.
+*/
 Caravan new_caravan()
 {
   return 0;
 }
 
+/**
+* @return Die Länge einer Karawane, d.h., die Anzahl der Tiere in der Karawane.
+*/
 int get_length(Caravan caravan)
 {
   return 0;
 }
 
+/**
+* Entfern alle Tiere aus einer Karawane und löscht die Karawane.
+*/
 void delete_caravan(Caravan caravan)
 {
+
 }
 
+/**
+* Fügt ein Packtier zu einer Karawane hinzu.
+*/
 void add_pack_animal(Caravan caravan, PackAnimal animal)
 {
+
 }
 
+/**
+* Entfernt ein Packtier aus einer Karawane.
+*/
 void remove_pack_animal(Caravan caravan, PackAnimal animal)
 {
+
 }
 
+/**
+* @return Die Geschwindigkeit einer Karawane. Diese richtet sich nach der Geschwindigkeit des
+* langsamsten Tiers in der Karawane.
+*/
+int get_caravan_speed(Caravan caravan)
+{
+  return 0;
+}
+
+/**
+* @return Die Gesamtanzahl der Ballen einer Karawane.
+*/
 int get_caravan_load(Caravan caravan)
 {
   return 0;
 }
 
+/**
+* Entl&auml;dt die gesamte Karawane. D.h. es wird jedes Tier in der Karawane von seinen Ballen
+* befreit.
+*/
 void unload(Caravan caravan)
 {
+
 }
 
-int get_caravan_speed(Caravan caravan)
+/**
+* Verteilt die Last der Ballen so auf die einzelnen Tiere, dass die Geschwindigkeit der Karawane
+* maximal ist.
+*/
+void optimize_load(Caravan caravan)
 {
-  return 0;
+  
 }
